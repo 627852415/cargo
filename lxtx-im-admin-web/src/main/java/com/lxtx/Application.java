cbff7fc32f7232381614dae822e44b02cfdeff36
haijun <494370771@qq.com>
2020-04-13 20:37:37 +0800

    remove test

--- lxtx-im-admin-web/src/main/java/com/lxtx/Application.java
+++ lxtx-im-admin-web/src/main/java/com/lxtx/Application.java
@@ -60,7 +60,7 @@ public class Application {
 		executor.setCorePoolSize(corePoolSize);
 		executor.setQueueCapacity(1000000);
 		executor.setKeepAliveSeconds(60);
-		// rejection-policy：当pool已经达到max size的时候，如何处理新任务 sds
+		// rejection-policy：当pool已经达到max size的时候，如何处理新任务
 		// CALLER_RUNS：不在新线程中执行任务，而是由调用者所在的线程来执行
 		executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
 		return executor;
